4539d14376c33f0c9bf64b3d9440fbc1d7f2d9db
Lalitkumar <lalitkumarpatil45@gmail.com>
2026-02-15 01:15:39 +0530

    user email added in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 function Profile() {
+  const { user } = useContext(AuthContext);
   return (
     <div style={{ padding: "20px" }}>
       <h3>User Profile</h3>
       <p style={{ padding: 0, margin: 0 }}>
-        <strong>Email:</strong> user@example.com
+        <strong>Email:</strong> {user.email}
       </p>
       <p style={{ padding: 0, margin: 0 }}>
         <strong>Account created on:</strong> --coming soon--
